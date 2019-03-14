6db38c86d22378f2004269ffcbb94c5b48f5cef2
Ragnhildur Rós <ragnhildurros@gmail.com>
2019-03-14 11:33:25 +0000

    ragnhildur page

---
+++ src/ragnhildur.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Ragnhildur extends Component{
+    render(){
+        return(
+            <div>Ragnhildur page</div>
+        )
+    }
+}
+
+export default Ragnhildur;
