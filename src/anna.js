70d69db4bf842ff0e13b052bf81f6b00a5cc88ed
Anna Sóley <annasoley09@gmail.com>
2019-03-14 11:34:02 +0000

    Anna Sóley

---
+++ src/anna.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Anna extends Component {
+    render() {
+        return(
+
+            <div>Anna Sóley </div>
+        )
+    }
+
+}
+
+
+export default Anna;
