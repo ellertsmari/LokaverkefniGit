88608484367e389b3f065e1f45e751b515f21072
Ragnhildur Rós <ragnhildurros@gmail.com>
2019-03-14 11:49:35 +0000

    mslkfjksfmdmds

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Ragnhildur from './ragnhildur.js';
-=======
-import Anna from '../LokaverkefniGit/src/anna'
-
->>>>>>> 8b722e1687f4570dd55a73fd079ede59642d90ac
 
 class App extends Component {
   render() {
